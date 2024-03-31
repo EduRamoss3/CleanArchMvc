09a8b25f5abcd345e9029e3d68975b05d399da7e
Eduardo Ramos <eduardoramossilva@live.com>
2024-03-30 21:36:02 -0300

    implementing and add concret class IProductRepository

--- CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Interfaces
 {
-    internal interface IProductRepository
+    public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetProductsAsync();
         Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
         Task<Product> CreateAsync(Product product);
         Task<Product> UpdateAsync(Product product);
         Task<Product> RemoveAsync(Product product);
+       
     }
 }
---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    internal class IProductRepository
+    {
+    }
+}
