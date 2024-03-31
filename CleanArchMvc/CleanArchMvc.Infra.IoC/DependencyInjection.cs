49bfd237a75a4489878003b99b539e63abae2ca5
EDUARDO RAMOS DA SILVA <100438022+EduRamoss3@users.noreply.github.com>
2024-03-31 14:02:02 -0300

    Update DependencyInjection.cs

--- CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Infra.IoC
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+            b=> b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryReposity, CategoryRepository>();
+            return services;
+        }
     }
 }
+
