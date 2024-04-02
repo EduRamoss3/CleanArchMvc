677edd971b43a3f98c4a96a956ec173e525c91f6
Eduardo Ramos <eduardoramossilva@live.com>
2024-04-01 22:43:34 -0300

    little update in interface IProductService

--- CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
+++ CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace CleanArchMvc.Application.Interfaces
 {
     public interface IProductService
     {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(ProductDTO product);
+        Task<Product> UpdateAsync(ProductDTO product);
+        Task<Product> RemoveAsync(ProductDTO product);
     }
 }
